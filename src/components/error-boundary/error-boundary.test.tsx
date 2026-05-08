848bd53c5425e0bca1e1dc6fb25ab45e127f957a
Fiodorova Maria <mariya.sg.rp@gmail.com>
2026-05-08 18:35:41 -0500

    feat: add tests for error boundary

---
+++ src/components/error-boundary/error-boundary.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ErrorBoundary } from './error-boundary';
+
+const ThrowError = () => {
+  throw new Error('Test error');
+};
+
+describe(`ErrorBoundry Component`, () => {
+  test(`renders children when there's no error`, () => {
+    render(
+      <ErrorBoundary>
+        <p>Example child node</p>
+      </ErrorBoundary>,
+    );
+
+    expect(screen.getByText('Example child node')).toBeInTheDocument();
+  });
+  test(`renders fallback UI when child throws error`, async () => {
+    const consoleError = vi.spyOn(console, 'error').mockImplementation(() => {});
+    const reload = vi.fn();
+    vi.spyOn(window, 'location', 'get').mockReturnValue({ ...window.location, reload });
+
+    const user = userEvent.setup();
+
+    render(
+      <ErrorBoundary>
+        <ThrowError />
+      </ErrorBoundary>,
+    );
+
+    const reloadButton = screen.getByRole('button', { name: 'Reload page' });
+
+    expect(screen.getByText('The app has crashed')).toBeInTheDocument();
+    expect(reloadButton).toBeInTheDocument();
+
+    await user.click(reloadButton);
+
+    expect(reload).toHaveBeenCalledTimes(1);
+
+    consoleError.mockRestore();
+  });
+});
