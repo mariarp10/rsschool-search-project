03a6e333641814e529e0f41659d305468c50e43f
Fiodorova Maria <mariya.sg.rp@gmail.com>
2026-05-18 17:07:49 -0500

    feat: add custom not found page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,9 +3,11 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Header } from '@ui/header/header';
 import { Footer } from '@ui/footer/footer';
 import { ErrorBoundary } from '@components/error-boundary';
+import { NotFoundPage } from '@pages/not-found';
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: NotFoundPage,
 });
 
 function RootComponent() {
